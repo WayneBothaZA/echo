095ca150d8f7ae361d1cafc67e915088ebdcc933
Wayne Botha <wayne@witklippies.co.za>
2021-08-07 17:44:45 +0200

    Added POD_NAME and return json response

--- echo.go
+++ echo.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
 	"log"
@@ -11,10 +12,22 @@ import (
 // The flag package provides a default help printer via -h switch
 var versionFlag *bool = flag.Bool("v", false, "print the version number.")
 
+var podName = ""
+
+type EchoResponse struct {
+	Message string `json:"message"`
+	PodName string `json:"podname"`
+}
+
 func echo(w http.ResponseWriter, r *http.Request) {
+	response := EchoResponse{
+		Message: "echo",
+		PodName: podName,
+	}
+	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("echo\n"))
-	log.Printf("/echo %d", http.StatusOK)
+	json.NewEncoder(w).Encode(response)
+	log.Printf("%s: /echo %d", podName, http.StatusOK)
 }
 
 func main() {
@@ -27,8 +40,10 @@ func main() {
 		os.Exit(0)
 	}
 
+	podName, _ = os.LookupEnv("POD_NAME")
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/echo", echo)
-	log.Printf("starting echo service on:%d", 8080)
+	log.Printf("starting echo service on %s:%d", podName, 8080)
 	log.Fatal(http.ListenAndServe(":8080", mux))
 }
