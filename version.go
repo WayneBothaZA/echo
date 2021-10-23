d2c17874dbf309c4733b08266f7fa633a66f5ba2
Wayne Botha <wayne@witklippies.co.za>
2021-10-23 15:34:48 +0200

    Added comments to solve golint warnings

--- version.go
+++ version.go
@@ -1,7 +1,16 @@
 package main
 
+// BuildVersion contains the version number of the build
 var BuildVersion = "0.0.0"
+
+// BuildUser contains the username of the user that created the build
 var BuildUser = "nobody"
+
+// BuildDate contains the date the build was created
 var BuildDate = "2020/01/01"
+
+// BuildTime contains the time the build was created
 var BuildTime = "00:00:00"
+
+// BuildCommit contains the Git commit number of the build
 var BuildCommit = "initial"
